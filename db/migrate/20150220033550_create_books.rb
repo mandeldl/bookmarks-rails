b3c66df6f0ac08b62028b4d2b675828afdb4ee2a
David Mandel <MandelDL@gmail.com>
2015-02-19 22:41:27 -0500

    update books model table

--- db/migrate/20150220033550_create_books.rb
+++ db/migrate/20150220033550_create_books.rb
@@ -1,7 +1,12 @@
 class CreateBooks < ActiveRecord::Migration
   def change
     create_table :books do |t|
-
+    	t.string :title
+    	t.string :author
+    	t.string :description
+    	t.string :publisher
+    	t.integer :weeks_on_list
+    	t.integer :rank_this_week
       t.timestamps
     end
   end
